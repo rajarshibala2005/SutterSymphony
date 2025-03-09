5219dce0ef6b3001c5530243eafdad7d08857704
rajarshibala2005 <balarajarshi@gmail.com>
2025-03-09 13:20:45 +0530

    animation added

---
+++ scripts/elementAnimeOnView.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sectionObserver = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        console.log(`Entry for ${entry.target.tagName}: isIntersecting = ${entry.isIntersecting}`);
+        if (entry.isIntersecting) {
+          console.log(`Element ${entry.target.tagName} is in view`);
+          entry.target.classList.add("visible");
+        } else {
+          console.log(`Element ${entry.target.tagName} is out of view`);
+          entry.target.classList.remove("visible");
+        }
+      });
+    }, { threshold: 0.4 });
+  
+    document.querySelectorAll(".fade-in, .fade-slide-top, .fade-slide-bottom, .logo-image, .fade-slide-left").forEach(element => {
+      console.log(`Observing element: ${element.tagName}`);
+      sectionObserver.observe(element);
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const sectionObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          console.log(`Element ${entry.target.tagName} is in view`);
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        } else {
+          console.log(`Element ${entry.target.tagName} is out of view`);
+          entry.target.classList.remove("visible");
+        }
+      });
+    }, { threshold: 0.9 });
+    
+    document.querySelectorAll(".aboutUs-anime").forEach(element => {
+      console.log(`Observing element: ${element.tagName}`);
+      sectionObserver.observe(element);
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const sectionObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        console.log(`Entry for ${entry.target.tagName}: isIntersecting = ${entry.isIntersecting}`);
+        if (entry.isIntersecting) {
+          console.log(`Element ${entry.target.tagName} is in view`);
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        } else {
+          console.log(`Element ${entry.target.tagName} is out of view`);
+        }
+      });
+    }, { threshold: 0 });
+  
+    document.querySelectorAll(".slide-left_2_right").forEach(element => {
+      console.log(`Observing element: ${element.tagName}`);
+      sectionObserver.observe(element);
+    });
+  });
